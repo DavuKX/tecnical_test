38bc48c98f2492b1971afd49065b1c56fe1896e4
DavuKX <duvandavid2001@gmail.com>
2022-09-02 19:55:02 -0500

    prepare the react app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,72 +1,13 @@
 import './App.css';
-import axios from 'axios';
-import React, {useEffect} from 'react';
-import { Link } from 'react-router-dom';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from 'react-router-dom';
-import Create from '.routes/create';
 
 const url = 'http://127.0.0.1:8000/api/person/';
 
-class App extends React.Component {
-
-  state = {
-    persons: [],
-  };
-
-  getPetition = () => {
-    axios.get(url)
-      .then(res => {
-        this.setState({persons: res.data});
-      }).catch(error => {
-        console.log(error);
-      }
-    );
-  }
-
-  componentDidMount() {
-    this.getPetition();
-  }
-
-  render() {
+function App() {
     return (
-      <div className="App">
-        <br/>
-        <button path="create" element={<create/>}>Agregar</button>
-        <br/><br/>
-        <table>
-          <thead>
-            <tr>
-              <th>Tipo de documento</th>
-              <th>Numero de documento</th>
-              <th>Nombre</th>
-              <th>Apellido</th>
-              <th>Hobby</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.persons.map(person => (
-              <tr key={person.id}>
-                <td>{person.document_type}</td>
-                <td>{person.document_number}</td>
-                <td>{person.first_name}</td>
-                <td>{person.last_name}</td>
-                <td>{person.hobbie}</td>
-                <yd>
-                  <button>Editar</button>
-                  <button>Eliminar</button>
-                </yd>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    );
-  }
+        <div className="App">
+            <h1>Hello</h1>
+        </div>
+      );
 };
-
 
 export default App;
