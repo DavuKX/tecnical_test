bfb7c156e4f1d06c56c794100c9b6e31ee2a7c70
DavuKX <duvandavid2001@gmail.com>
2022-09-02 19:34:50 -0500

    started reac app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,50 +1,72 @@
 import './App.css';
+import axios from 'axios';
 import React, {useEffect} from 'react';
+import { Link } from 'react-router-dom';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import Create from '.routes/create';
 
+const url = 'http://127.0.0.1:8000/api/person/';
 
-function App() {
-  
-  const url = 'http://127.0.0.1:8000/api/person/';
-  const [persons, setPersons] = React.useState([]);
+class App extends React.Component {
 
-  // get data from api
-  const fetchData = async () => {
-  const response = await fetch(url);
-  const data = await response.json();
-  setPersons(data);
-  return data;
-}
+  state = {
+    persons: [],
+  };
 
-  useEffect(() => {
-    fetchData() 
-  }, [])
-  
-  return (
-    <div className="App">
-      <tr>
-        <th>Tipo de documento</th>
-        <th>Numero de documento</th>
-        <th>Nombre</th>
-        <th>Apellido</th>
-        <th>Hobby</th>
-      </tr>
-      <ul>
-        { !persons ? <h1>Loading...</h1> : 
-        persons.map((person, index) => {
-            return (
-              <tr key={index}>
+  getPetition = () => {
+    axios.get(url)
+      .then(res => {
+        this.setState({persons: res.data});
+      }).catch(error => {
+        console.log(error);
+      }
+    );
+  }
+
+  componentDidMount() {
+    this.getPetition();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <br/>
+        <button path="create" element={<create/>}>Agregar</button>
+        <br/><br/>
+        <table>
+          <thead>
+            <tr>
+              <th>Tipo de documento</th>
+              <th>Numero de documento</th>
+              <th>Nombre</th>
+              <th>Apellido</th>
+              <th>Hobby</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.persons.map(person => (
+              <tr key={person.id}>
                 <td>{person.document_type}</td>
                 <td>{person.document_number}</td>
                 <td>{person.first_name}</td>
                 <td>{person.last_name}</td>
                 <td>{person.hobbie}</td>
+                <yd>
+                  <button>Editar</button>
+                  <button>Eliminar</button>
+                </yd>
               </tr>
-            )
-          })
-        }    
-      </ul>
-    </div>
-  );
-}
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+};
+
 
 export default App;
